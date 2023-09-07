a69f66f756dabbedfc113135e6bd31e663d2a0ef
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-09-07 23:14:52 +0530

    deployment link

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://sharedocs-api.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
