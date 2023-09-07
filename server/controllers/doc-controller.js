f3b678d7653c603899fcf98fa943c03add527586
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-09-07 23:19:23 +0530

    deployment link

--- server/controllers/doc-controller.js
+++ server/controllers/doc-controller.js
@@ -12,7 +12,7 @@ export const uploadDoc = async (req, res) => {
     try {
         const newFile = await File.create(fileObj)
 
-        const downloadlink = `http://localhost:8080/files/download/${newFile._id}`;
+        const downloadlink = `https://sharedocs-api.onrender.com/files/download/${newFile._id}`;
 
         newFile.downloadlink = downloadlink;
         await newFile.save();
@@ -21,20 +21,6 @@ export const uploadDoc = async (req, res) => {
     } catch (error) {
         console.error(error.message)
         res.status(500).json({ error: error.message })
-    }
-}
-
-export const downloadImage = async (req, res) => {
-    try {
-        const file = await File.findById(req.params.fileId)
-
-        file.downloadCount++;
-        await file.save();
-
-        res.download(file.path, file.name);
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).json({ error: error.message })
     }
 }
 
