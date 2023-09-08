f1a9c39c36634b3c6c8001a955f02097c7a4a107
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-09-08 10:55:52 +0530

    done

--- client/src/components/DocLists.jsx
+++ client/src/components/DocLists.jsx
@@ -1,20 +1,25 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { IconButton } from '@mui/material';
+import { CircularProgress, IconButton } from '@mui/material';
 import { ContentPaste, Download, FileCopy } from '@mui/icons-material';
 
 const DocLists = ({ files, setFiles, scrollRef }) => {
     const [filteredFiles, setFilteredFiles] = useState([]);
     const [searchInput, setSearchInput] = useState('');
     const [copyFileId, setCopyFileId] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         async function getAllFiles() {
             try {
+                setLoading(true);
                 const { data } = await axios.get('/files');
                 setFiles(data);
+                setLoading(false);
             } catch (error) {
                 console.error('Error fetching files:', error);
+                setError(error.message);
             }
         }
 
@@ -96,36 +101,47 @@ const DocLists = ({ files, setFiles, scrollRef }) => {
 
 
     return (
-        <div className='DocListWrapper bg-white shadow h-96 w-[98%] mx-auto mt-6 ring ring-gray-300 ring-opacity-50 overflow-y-hidden'>
-            <div className='flex justify-between items-center px-2 py-1'>
-                <h1 className='text-lg font-semibold'>counts: {!searchInput ? files?.length : filteredFiles.length}</h1>
-                <input
-                    type="text"
-                    className='SearchInput border h-[32px] w-40 sm:w-52 border-b-gray-500 text-lg px-1 py-1 outline-none mr-2'
-                    placeholder="Search any file"
-                    value={searchInput}
-                    onChange={(e) => setSearchInput(e.target.value)}
-                />
+
+        loading ?
+            <div className='absolute top-[60%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+                {!error ? (
+                    <div className='flex flex-col justify-center items-center'>
+                        < CircularProgress />
+                        Loading...
+                    </div>) : error
+                }
+            </div > :
+
+            <div className='DocListWrapper bg-white shadow h-96 w-[98%] mx-auto mt-6 ring ring-gray-300 ring-opacity-50 overflow-y-hidden'>
+                <div className='flex justify-between items-center px-2 py-1'>
+                    <h1 className='text-lg font-semibold'>counts: {!searchInput ? files?.length : filteredFiles.length}</h1>
+                    <input
+                        type="text"
+                        className='SearchInput border h-[32px] w-40 sm:w-52 border-b-gray-500 text-lg px-1 py-1 outline-none mr-2'
+                        placeholder="Search any file"
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                    />
+                </div>
+                <hr />
+                <div className="DocumentList px-2 max-h-80 overflow-x-hidden overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+                    <table className="w-full">
+                        <thead className="bg-gray-200 text-center sm:text-left sticky top-0 z-10">
+                            <tr>
+                                <th className="py-2 px-4">Document Name</th>
+                                <th className="py-2 px-4 hidden md:table-cell">Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {!searchInput ? (
+                                files?.map((file, index) => <File file={file} key={index} />)
+                            ) : (
+                                filteredFiles?.map((file, index) => <File file={file} key={index} />)
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <hr />
-            <div className="DocumentList px-2 max-h-80 overflow-x-hidden overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
-                <table className="w-full">
-                    <thead className="bg-gray-200 text-center sm:text-left sticky top-0 z-10">
-                        <tr>
-                            <th className="py-2 px-4">Document Name</th>
-                            <th className="py-2 px-4 hidden md:table-cell">Description</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {!searchInput ? (
-                            files?.map((file, index) => <File file={file} key={index} />)
-                        ) : (
-                            filteredFiles?.map((file, index) => <File file={file} key={index} />)
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        </div>
     );
 };
 
